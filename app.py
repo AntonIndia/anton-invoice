ae09bd4c063ccf0362d00a2c4a5563d65d4d2527
Anton <done@antonclothing.com>
2025-01-17 23:45:32 +0530

    Fix streamlit app deployment

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Anton Clothing - DC to Invoice")
+st.write("Welcome to Invoice Generator")
+
+# Add a simple test button
+if st.button('Test Button'):
+    st.write('App is working!')
